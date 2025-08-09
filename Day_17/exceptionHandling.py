f57c67975f9ab82112b3cf17d86da5d4431c4d58
codingtacker <benicioallaglo@gmail.com>
2025-08-09 22:25:56 +0000

    Day 17 of python challenge

---
+++ Day_17/exceptionHandling.py
@@ -0,0 +1,9 @@
+ # Jour 17 : 30 Days of Python Programming
+
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+*nordic_countries, es, ru = names
+
+print("Nordic countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
